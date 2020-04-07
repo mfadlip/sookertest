af5a2fc78a4276ae21a569b259c0d71c6c9d614f
mfadlip <52195502+mfadlip@users.noreply.github.com>
2020-04-07 21:13:22 +0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,17 @@
+<div  class="container pt-5">
+	<div class="row">
+		<style>
+			body {
+			  background-image: url('sooker3.jpg');
+			  background-repeat: no-repeat;
+			  background-attachment: fixed;  
+			  background-size: cover;
+				}
+		</style>
+		<div align="center" class="col-md-12">
+				<h1 class="display-4">Gosah Sooker</h1>
+				<p class="lead">Jangan Menua Tanpa Cerita dan Cinta.</p>
+				<a class="btn btn-lg btn-success" href="#" role="button">Telusuri</a>	
+		</div>
+	</div>
+</div>
